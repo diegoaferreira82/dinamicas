c030003ac5144ef063a37d3f68d2d2e37579dec9
diegoaferreira82 <diegoaferreira82@hotmail.com>
2026-02-02 15:52:56 -0400

    Ultimo

--- main.js
+++ main.js
@@ -5,13 +5,18 @@
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function tocaSom(idElementAudio){
-    document.querySelector(idElementAudio).play();
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+    //alert(elemento.localName);
+    if (elemento && elemento.localName==='audio') {
+        elemento.play();
+    } else {
+        //quando compara somente elemento, js entende que se não é nulo, undefined, etc
+        alert('Elemento não encontrado!');
+    }
 }
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador=0; contador < listaDeTeclas.length; contador++) {
     
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
@@ -20,30 +25,21 @@ while (contador < listaDeTeclas.length) {
     tecla.onclick = function() { //função anonima, se passar direto a função tocasom o browser vai tentar executar e dara erro pois ainda nao teve interação do usuario
         tocaSom(idAudio);
     }
-    contador++;
+
+    tecla.onkeydown = function (evento){
+        if ((evento.code == 'Space') || (evento.code == 'Enter')) {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
 }
 
 
 //sem o parentese para não executar, apenas guardar a referencia 
 
-
-//function tocaSom (idElementoAudio) {
-//    document.querySelector(idElementoAudio).play();
-//}
-//
-//for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-//
-//    const tecla = listaDeTeclas[contador];
-//    const instrumento = tecla.classList[1];
-//    const idAudio = `#som_${instrumento}`; //template string
-//
-//    tecla.onclick = function () {
-//        tocaSom(idAudio);
-//    }
-//
-//}
-
-//Aqui nao funcionou
+//Aqui nao funcionou PQ???
 //function tocaSom(idElementAudio){
 //    document.querySelector(`#som_${idElementAudio}`).play();
 //}
@@ -52,7 +48,7 @@ while (contador < listaDeTeclas.length) {
 //
 //while (contador < listaDeTeclas.length) {
 //    listaDeTeclas[contador].onclick = function() { 
-//        tocaSom(listaDeTeclas[contador].classList[1]);
+//        tocaSom(listaDeTeclas[contador].classList[1]);  //pega a referência e não o valor da variável???
 //    }
 //    contador++;
 //}
